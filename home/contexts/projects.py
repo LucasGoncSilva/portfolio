a11c4a7d261406150546ea9709269c6c92d36bf9
Lucas Gonçalves da Silva <lucasgoncsilva04@gmail.com>
2022-05-12 12:42:52 -0300

    removing wrong character from links in projects url

--- home/contexts/projects.py
+++ home/contexts/projects.py
@@ -25,31 +25,31 @@ class Project:
 lg = Project('lg', 'Lucas Gonc')
 lg.description = 'Nada mais, nada menos que este próprio website - meu portfólio. Construído do zero e utilizando Django - um Framework Python com arquitetura MVC - como fundamento, este site apresenta parte daquilo que sou capaz de fazer.'
 lg.github = 'lucasgonc'
-lg.project_link = 'https://lucasgonc.herokuapp.com/'
+lg.project_link = 'https://lucasgonc.herokuapp.com'
 
 
 task_list = Project('task_list', 'Gerenciador de Tarefas')
 task_list.description = 'Uma lista pessoal de tarefas que armazena as informações no local storage, dispensando o uso de base de dados e possibilitando o acesso a cada item da lista sempre que necessário. Uma vez que não existe base de dados, não há como acessar uma mesma lista em diferentes aparelhos através de login.'
 task_list.github = 'task_list'
-task_list.project_link = 'https://personaltasklist.vercel.app/'
+task_list.project_link = 'https://personaltasklist.vercel.app'
 
 
 passgen = Project('pass_gen', 'Gerador de Senhas')
 passgen.description = 'Desenvolvido em JavaScript e utilizando a framework Electron para a geração da interface gráfica, este gerador de senhas possibilita que o usuário escolha a quantidade de caracteres que deseja que a senha contenha, juntamente aos tipos de caracteres (minúsculas, maiúsculas, números e/ou símbolos.'
 passgen.github = 'password_generator'
-passgen.project_link = 'https://gerarsenhas.vercel.app/'
+passgen.project_link = 'https://gerarsenhas.vercel.app'
 
 
 clock = Project('clock', 'Relógio Digital')
 clock.description = 'Uma aplicação simples que informa o horário em tempo real com menos de um segundo de variação em relação ao horário oficial, o Digital Clock representa exatamente o que seu nome indica. Utiliza tecnologia web com Electron como framework para desktop app.'
 clock.github = 'digital_clock'
-clock.project_link = 'https://simpledigitalclock.vercel.app/'
+clock.project_link = 'https://simpledigitalclock.vercel.app'
 
 
 encoder = Project('encoder', 'Encoder')
 encoder.description = 'Encoder (codificador em inglês, no contexto de criptografia) é um programa, dispositivo ou equipamento capaz de criptografar informações utilizando uma chave escolhida pelo usuário. Somente será revelada a verdadeira mensagem utilizando exatamente a mesma chave utilizada para gerar o texto codificado, caso contrário, a mensagem revelada será diferente de modo a não haver ligação com a mensagem original, nem mesmo sendo revelada mensagem alguma.'
 encoder.github = 'encoder'
-encoder.project_link = 'https://codificador.vercel.app/'
+encoder.project_link = 'https://codificador.vercel.app'
 
 
 cypher = Project('cypher', 'Cypher')
