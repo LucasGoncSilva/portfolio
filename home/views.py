27297af9d6008198a068ef7bf2a1f1f0ac593d19
Lucas Gonçalves da Silva <lucasgoncsilva04@gmail.com>
2022-05-22 13:16:56 -0300

    TLS to SSL

--- home/views.py
+++ home/views.py
@@ -27,16 +27,16 @@ def index(request: object) -> object:
 
 def mail(request: object) -> object:
     if request.method == 'POST':
-        # name = request.POST.get('name')
-        # email = request.POST.get('email')
-        # message = request.POST.get('message')
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
 
-        # send_mail(
-        #     'Portfolio Contact Made',
-        #     f'from: {name}\nemail: {email}\n\n\n{message}',
-        #     EMAIL_HOST_USER,
-        #     [EMAIL_ADM]
-        # )
+        send_mail(
+            'Portfolio Contact Made',
+            f'from: {name}\nemail: {email}\n\n\n{message}',
+            EMAIL_HOST_USER,
+            [EMAIL_ADM]
+        )
 
         request.session['email_sent'] = True
 
