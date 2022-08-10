e416da80dfc6691df6035bd2d5174e48e94d7e6c
Lucas Gonçalves da Silva <lucasgoncsilva04@gmail.com>
2022-08-09 22:32:23 -0300

    projects images refactored

--- home/contexts/projects.py
+++ home/contexts/projects.py
@@ -22,7 +22,7 @@ class Project:
         return self.project_link
 
 
-lg = Project('lg', 'Lucas Gonc')
+lg = Project('portfolio', 'Lucas Gonc')
 lg.description = 'Nada mais, nada menos que este próprio website - meu portfólio. Construído do zero e utilizando Django - um Framework Python com arquitetura MVC - como fundamento, este site apresenta parte daquilo que sou capaz de fazer.'
 lg.github = 'portfolio'
 lg.project_link = 'https://lucasgonc.herokuapp.com'
@@ -40,22 +40,4 @@ task_list.github = 'task_list'
 task_list.project_link = 'https://personaltasklist.vercel.app'
 
 
-passgen = Project('pass_gen', 'Gerador de Senhas')
-passgen.description = 'Desenvolvido em JavaScript e utilizando a framework Electron para a geração da interface gráfica, este gerador de senhas possibilita que o usuário escolha a quantidade de caracteres que deseja que a senha contenha, juntamente aos tipos de caracteres (minúsculas, maiúsculas, números e/ou símbolos.'
-passgen.github = 'password_generator'
-passgen.project_link = 'https://gerarsenhas.vercel.app'
-
-
-encoder = Project('encoder', 'Encoder')
-encoder.description = 'Encoder (codificador em inglês, no contexto de criptografia) é um programa, dispositivo ou equipamento capaz de criptografar informações utilizando uma chave escolhida pelo usuário. Somente será revelada a verdadeira mensagem utilizando exatamente a mesma chave utilizada para gerar o texto codificado, caso contrário, a mensagem revelada será diferente de modo a não haver ligação com a mensagem original, nem mesmo sendo revelada mensagem alguma.'
-encoder.github = 'encoder'
-encoder.project_link = 'https://codificador.vercel.app'
-
-
-cypher = Project('cypher', 'Cypher')
-cypher.description = 'Baseado na "Cifra de César" com algumas modificações pessoais, este programa com interface gráfica possibilita a tradução ida e volta entre um texto/mensagem normal e cifrado. A matemática por trás da tradução é simples o suficiente para que seja possivel traduzir mensagens sem a necessidade de um programa, utilizando apenas a própria mente. Com base nisso, o Cypher foi desenvolvido para facilitar e acelerar a transcrição de textos longos para troca de informações.'
-cypher.github = 'cypher'
-cypher.project_link = 'https://textcypher.vercel.app'
-
-
-project_cards = [lg, lipsum, task_list, passgen, encoder, cypher]
+project_cards = [lg, lipsum, task_list]
