d07fde41681febddb9ade93d09c4cc92d18d0335
Lucas Gonçalves da Silva <lucasgoncsilva04@gmail.com>
2023-03-09 14:13:10 -0300

    new deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,32 @@
-echo "Starting deploy build"
+echo "
+
+Starting deploy build"
+echo "___________________________"
+
+
+echo "
+___________________________"
+echo "Downloading requirements"
+echo "___________________________"
 python3.9 -m pip install -r requirements.txt
+
+
+echo "
+___________________________"
+echo "Migrating DB"
+echo "___________________________"
 python3.9 manage.py makemigrations
 python3.9 manage.py migrate
+
+
+echo "
+___________________________"
+echo "Collecting static files (css, img, js)"
+echo "___________________________"
 python3.9 manage.py collectstatic --noinput --clear
-echo "Ending deploy build"
+
+
+echo "
+
+Ending deploy build"
+echo "___________________________"
