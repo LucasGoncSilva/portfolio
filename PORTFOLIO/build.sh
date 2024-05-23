225f591bc58ccb8085bd3e949f8dcdb5dbc82b2c
Lucas Gonçalves da Silva <lucasgoncsilva04@gmail.com>
2024-05-22 23:47:00 -0300

    set build.sh to store one-line cmd

--- PORTFOLIO/build.sh
+++ PORTFOLIO/build.sh
@@ -10,3 +10,7 @@ python manage.py collectstatic --no-input
 # Handles DB migrations
 python manage.py makemigrations
 python manage.py migrate
+
+
+# set -o errexit; pip install -r requirements.txt; python manage.py collectstatic --no-input; python manage.py makemigrations; python manage.py migrate
+
