c38b84b816944c39f7d5fb5f9b3cffc3c60fad36
Lucas Gonçalves da Silva <lucasgoncsilva04@gmail.com>
2024-05-27 10:09:27 -0300

    configs deploy.sh

--- PORTFOLIO/deploy.sh
+++ PORTFOLIO/deploy.sh
@@ -2,14 +2,14 @@
 set -o errexit
 
 # Installs dependencies
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 
 # Collects staticfiles
-python manage.py collectstatic --no-input
+python3 manage.py collectstatic --no-input
 
 # Handles DB migrations
-python manage.py makemigrations
-python manage.py migrate
+python3 manage.py makemigrations
+python3 manage.py migrate
 
 
 # set -o errexit; pip install -r requirements.txt; python manage.py collectstatic --no-input; python manage.py makemigrations; python manage.py migrate
