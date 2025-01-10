a5260cdb2789c477bc6c32557076afe406d1734b
Lucas Gonçalves da Silva <lucasgoncsilva04@gmail.com>
2025-01-10 10:29:54 -0300

    tryna fix deploy.sh

--- PORTFOLIO/deploy.sh
+++ PORTFOLIO/deploy.sh
@@ -3,10 +3,8 @@
 pip3 install uv
 python3 -m uv -h
 
-uv sync --no-group dev
+python3 -m uv sync --no-group dev
 
-find manage.py
-
-uv run manage.py collectstatic --no-input
-uv run manage.py makemigrations
-uv run manage.py migrate
+python3 -m uv run manage.py collectstatic --no-input
+python3 -m uv run manage.py makemigrations
+python3 -m uv run manage.py migrate
