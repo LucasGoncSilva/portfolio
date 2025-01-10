ed433c7847dea138d7e5e1feab0980c1c998c7a0
Lucas Gonçalves da Silva <lucasgoncsilva04@gmail.com>
2025-01-10 14:21:08 -0300

    tryna fix deploy.sh

--- PORTFOLIO/deploy.sh
+++ PORTFOLIO/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 pip3 install uv
-python3 -m uv -h
 python3 -m uv sync --no-group dev
 python3 -m uv run manage.py collectstatic --no-input
 python3 -m uv run manage.py makemigrations
 python3 -m uv run manage.py migrate
+python3 -m uv pip compile pyproject.toml -o requirements.txt
