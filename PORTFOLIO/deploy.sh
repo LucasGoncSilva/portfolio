0b066f2760924a80eee0d5e8222523ec0775b4a1
Lucas Gonçalves da Silva <lucasgoncsilva04@gmail.com>
2025-01-09 23:16:27 -0300

    tryna fix deploy.sh

--- PORTFOLIO/deploy.sh
+++ PORTFOLIO/deploy.sh
@@ -1,3 +1,12 @@
-pip3 install uv
+#!/bin/bash
 
-bash -c "uv sync --no-group dev; uv run manage.py collectstatic --no-input; uv run manage.py makemigrations; uv run manage.py migrate;"
+python3 -m venv .venv
+source .venv/bin/activate
+
+pip3 install uv
+uv sync --no-group dev
+
+uv run manage.py collectstatic --no-input
+
+uv run manage.py makemigrations
+uv run manage.py migrate
